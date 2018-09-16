10ef26a9decf80a7f3d87ac21074291c8e5a2cc7
Mohammad Wasil <mwasil@outlook.co.id>
2018-09-16 16:50:04 +0000

    add function get_db_students

--- config/jupyter/nbgrader_config.py
+++ config/jupyter/nbgrader_config.py
@@ -1,5 +1,17 @@
-# Configuration file for nbgrader.
+# Some functions
 
+def get_db_students():    
+    db = [dict(id="student1", first_name="Ben", last_name="Bitdiddle"),
+    dict(id="student2", first_name="Alyssa", last_name="Hacker"),
+    dict(id="student3", first_name="Louis", last_name="Reasoner")]
+    return db
+
+# create a function for sharing course detail
+def get_course_detail():
+    return [course_name, course_id] 
+
+
+# Configuration file for nbgrader.
 c = get_config()
 course_name = 'mrc'
 course_id = 'mrc-ws1819'
@@ -12,7 +24,7 @@ c.Exchange.root = '/srv/nbgrader/exchange'
 
 c.CourseDirectory.feedback_directory = 'feedback'
 
-# create a function for sharing course detail
-def get_course_detail():
-    return [course_name, course_id] 
+# add student database here
+c.CourseDirectory.db_students = get_db_students()
+
 
