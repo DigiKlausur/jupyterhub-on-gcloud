4277669bc4415165c8fd3d7a601d31b6ad087c79
Mohammad Wasil <mwasil@outlook.co.id>
2018-09-20 13:15:42 +0000

    add restricted commands for students

---
+++ config/jupyter/restricted_commands.sh
@@ -0,0 +1,21 @@
+alias apt-get="printf ''"  #the user won't be able to run this  
+alias aptitude="printf ''"  #the user won't be able to run this  
+alias bash="printf ''"  #the user won't be able to run this  
+
+alias nano="printf ''"
+alias emacs="printf ''"
+alias pico="printf ''"
+alias sed="printf ''"
+alias vi="printf ''"
+alias vim="printf ''"
+
+alias cd
+#alias cp
+alias mv
+alias ls
+alias ll
+alias mkdir
+alias tree
+alias cat
+alias less
+alias more
