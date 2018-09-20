11550106ef338058b5a2feec1afcd780d2aa64a9
Mohammad Wasil <mwasil@outlook.co.id>
2018-09-20 13:14:24 +0000

    update release_feedback script

--- config/release_feedback.py
+++ config/release_feedback.py
@@ -1,7 +1,29 @@
 import grp
 import pwd
 import os
+import argparse
+from distutils import dir_util
+
 #from "/etc/jupyter/nbgrader_config.py" import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--assignment_id', default='', help='Assignment id')
+parser.add_argument('--student_id', default='', help='Student id')
+parser.add_argument('--student_group', default='mrc-student-group', help='Student group name')
+parser.add_argument('--student_fb_dir', default='feedback', help='Student feeback directory')
+
+FLAGS = parser.parse_args()
+ASSIGNMENT_ID = FLAGS.assignment_id
+STUDENT_ID = FLAGS.student_id
+STUDENT_GROUP = FLAGS.student_group
+STUDENT_FB_DIR = FLAGS.student_fb_dir
+
+course_name = "mrc"
+course_id = "mrc-ws1819"
+course_root = "/home/mrc-grader/mrc/mrc-ws1819"
+submitted_dir = "submitted"
+autograded_dir = "autograded"
+release_dir = "release"
 
 def add_group(group_name, system_group=False, gid=None):
     """Add a group to the system
@@ -26,18 +48,6 @@ def add_group(group_name, system_group=False, gid=None):
         group_info = grp.getgrnam(group_name)
     return group_info 
 
-course_name = "mrc"
-course_id = "mrc-ws1819"
-course_root = "/home/mrc-grader/mrc/mrc-ws1819"
-feedback_dir = "feedback"
-submitted_dir = "submitted"
-autograded_dir = "autograded"
-release_dir = "release"
-assignment_id = "ps1"
-group = grp.getgrnam('mrc-student-group')
-print "Group data: ", group
-print group.gr_name
-print group.gr_mem
 
 # Check whether directory existance for submitted, autograded and feedback
 # e.g. check if all student submitted assigments: check_dir_exist(submitted_dir, assignment_id)
@@ -52,26 +62,51 @@ def check_dir_exist(directory, assignment_id):
             print os.path.join(course_root, directory, student, assignment_id), " exist..."
             return True
 
+# Check feedback
+# input: path to feedback directory
+def check_and_release_feedback(course_root, feedback_dir, student, assignment_id):
+    if os.path.exists(os.path.join(course_root, feedback_dir, student, assignment_id)):
+        assignment_fb_dir = os.path.join(course_root, feedback_dir, student, assignment_id)
+        # copy feedback to /home/student/feedback/assignment_id
+        dir_util.copy_tree(assignment_fb_dir,
+               os.path.join("/home", student, feedback_dir, assignment_id))
+    else:
+        print "Feedback not available for ", student
+        print "Do nbgrader feedback assignment_id in terminal, check if the student submitted the assignment"	
+
 # dir_util makes sure the folder copied to dst
 # if dst does not exist, it will create one
-from distutils import dir_util
 # Perform a recursive copy from src to dst
-def release_feedback():
-    for student in group.gr_mem:
-	# feedback
-	if os.path.exists(os.path.join(course_root, feedback_dir, student, assignment_id)):
-	    assignment_fb_dir = os.path.join(course_root, feedback_dir, student, assignment_id)
-	    # copy feedback to /home/student/feedback/assignment_id
-	    dir_util.copy_tree(assignment_fb_dir,
-			   os.path.join("/home", student, feedback_dir, assignment_id))	
-	
-        # check the existance of student feedback
-	#if os.path.exists(os.path.join("/home", student, course_id, assignment_id, feedback_dir)):
-	#    print "fb folder exist"
-	#else:
-	#    print "fb folder doesnot exist"
+def release_feedback(assignment_id, student_id, feedback_dir, student_group):  
+    if student_id == "":
+        for student in student_group:
+            # feedback
+            # TODO
+            # Check and release all feedback present in the course feedback dir
+            check_and_release_feedback(course_root, feedback_dir, student, assignment_id) 
+    else:
+        # TODO
+        # release fb to student_id only
+        check_and_release_feedback(course_root, feedback_dir, student_id, assignment_id)
 
-release_feedback()
+def main():
+    if ASSIGNMENT_ID == "":
+	    print "Please specify assignment id"
+	    return
+    
+    if STUDENT_ID == "":
+	    print "No student_id specified, will be applied to all students if the feedbacks exist in the course dir"
+    
+    if STUDENT_GROUP == "":
+	    print "No student group specified" 
+    group = grp.getgrnam('mrc-student-group')
+    #print "Group data: ", group
+    student_group = group.gr_mem
+    release_feedback(ASSIGNMENT_ID, STUDENT_ID, STUDENT_FB_DIR, student_group)
+    
+if __name__ == '__main__':
+    main()    
+#release_feedback()
 
 #check_dir_exist(feedback_dir, "ps1")
 
